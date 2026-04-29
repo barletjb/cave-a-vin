d9eec0cb5ef22a60b93b0986df00f4ffa36a89f3
jbarlet2025 <barletjb.perso@gmail.com>
2026-04-29 09:50:53 +0200

    :sparkles:[FEATURE](Ticket-14): Add getBottle

--- src/main/java/fr/eni/caveavin/controller/BouteilleController.java
+++ src/main/java/fr/eni/caveavin/controller/BouteilleController.java
@@ -2,33 +2,48 @@ package fr.eni.caveavin.controller;
 
 import fr.eni.caveavin.dao.BouteilleRepository;
 import fr.eni.caveavin.entity.vin.Bouteille;
+import fr.eni.caveavin.service.BouteilleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/caveavin")
+@RequestMapping("/api/caveavin/bouteilles")
 @RequiredArgsConstructor
 public class BouteilleController {
 
-    private final BouteilleRepository bouteilleRepository;
+    private final BouteilleService bouteilleService;
 
 
-    @GetMapping("/bouteilles")
-    public ResponseEntity<?> getAllBottles(){
+    @GetMapping
+    public ResponseEntity<?> getAllBottles() {
 
-        List<Bouteille> bouteilleList = bouteilleRepository.findAll();
+        List<Bouteille> bouteilleList = bouteilleService.chargerToutesBouteilles();
 
-        if(bouteilleList == null | bouteilleList.isEmpty() ){
-            return ResponseEntity.noContent().build();
+        if (bouteilleList == null || bouteilleList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
         return ResponseEntity.ok(bouteilleList);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getBottle(@PathVariable("id") String id) {
 
+        try {
+            int idBouteille = Integer.parseInt(id);
+            return ResponseEntity.ok(bouteilleService.chargerBouteilleParId(idBouteille));
+
+        } catch (NumberFormatException e){
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("L'id doit etre un entier ");
+        } catch (RuntimeException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
 }
